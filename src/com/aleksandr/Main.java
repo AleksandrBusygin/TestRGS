a6527eb4f05ed9efaf70f171e18835946561da29
Aleksandr Busygin <persik665@yandex.ru>
2019-03-18 23:35:11 +0300

    финальная версия

--- src/com/aleksandr/Main.java
+++ src/com/aleksandr/Main.java
@@ -22,19 +22,23 @@ public class Main {
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get("https://www.rgs.ru");
     }
+
     public static void compareText(String actual, String expected){
-        if (actual.contains(expected))
-        {
+        if (actual.contains(expected)) {
             System.out.println("Искомый текст есть: " + expected);
         }
-        else System.err.println("Искомого текста нет: " + expected + "вместо него" + actual);
-        driver.close();
+        else {
+            System.err.println("Искомого текста нет: " + expected + " вместо него " + actual);
+            driver.close();
+        }
     }
+
     public static void fillForm(By locator, String text){
         driver.findElement(locator).click();
         driver.findElement(locator).clear();
         driver.findElement(locator).sendKeys(text);
     }
+
     public static String setData(){
         SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy");
         Calendar i = Calendar.getInstance();
@@ -44,58 +48,103 @@ public class Main {
         return newDate;
     }
 
+    public static void checkBoxCheck(WebElement checkbox){
+        if(!checkbox.isSelected()){
+            checkbox.click();
+        }
+    }
+
+    public static void waitFieldisDisplayed(By locator) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, 10);
+            wait.until((WebDriver d) -> d.findElement(locator).isDisplayed());
+            return;
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Поле не отображено");
+    }
+
+    public static boolean isElementPresented(By locator) {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, 10);
+            wait.until((WebDriver d) -> d.findElement(locator));
+            return true;
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static void click(By locator) {
+        if (!isElementPresented(locator)) {
+        }
+        waitFieldisDisplayed(locator);
+        driver.findElement(locator).click();
+    }
+
+    public static void scrollToElement(WebElement find){
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", find);
+        Wait <WebDriver> wait = new WebDriverWait(driver, 20, 1000);
+        wait.until(ExpectedConditions.visibilityOf(find));
+    }
+
     public static void main(String[] args) {
         System.out.println("Тестовое задание для сайта РГС");
         setup();
-        String name ="IVANOV IVAN";
-        String dateOfBirth = "25101994";
+        click(By.xpath("//ol/li/a[contains(text(),'Страхование')]"));
 
-        driver.findElement(By.xpath("//ol/li/a[contains(text(),'Страхование')]")).click();
+        click(By.xpath("//*[@class='hidden-xs'][contains(text(),'Путешествия')]"));
 
-        driver.findElement(By.xpath("//*[@class='hidden-xs'][contains(text(),'Путешествия')]")).click();
-
-        driver.findElement(By.xpath("//*[contains(text(), 'Страхование выезжающих за')]")).click();
+        click(By.xpath("//*[contains(text(), 'Страхование выезжающих за')]"));
 
         WebElement element = driver.findElement(By.xpath("//a[contains(text(), 'Рассчитать')]"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();",element);
-        driver.findElement(By.xpath("//a[contains(text(), 'Рассчитать')]")).click();
+        scrollToElement(element);
+        click(By.xpath("//a[contains(text(), 'Рассчитать')]"));
 
         compareText(driver.findElement(By.xpath("//div/*[contains(text(), 'Страхование выезжающих')]")).getText(),"Страхование выезжающих за рубеж");
-//      здесь ломается
-        JavascriptExecutor js = (JavascriptExecutor)driver;
-        js.executeScript("scroll(0, 300);");
-        Wait <WebDriver> wait = new WebDriverWait(driver, 10, 1000);
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//span[contains(text(), 'Несколько')]"))));
-        driver.findElement(By.xpath("//span[contains(text(), 'Несколько')]")).click();
-        //button/*[contains(@class, 'content-title')] можно эту кнопку, но и с ней не работает
+
+        WebElement element1 = driver.findElement(By.xpath("//button/*[contains(@class, 'content-title')]"));
+        scrollToElement(element1);
+        click(By.xpath("//button/*[contains(@class, 'content-title')]"));
 
         fillForm(By.xpath("//*[@id='Countries']"),"Шенген");
+        WebElement element2 = driver.findElement(By.xpath("//*[@id='Countries']"));
+        element2.sendKeys(Keys.DOWN,Keys.ENTER);
 
-        driver.findElement(By.xpath("//input[@class='form-control-multiple-autocomplete-actual-input tt-input collapsed']")).click();
         new Select(driver.findElement(By.name("ArrivalCountryList"))).selectByVisibleText("Испания");
 
         driver.findElement(By.xpath("//input[@data-test-name='FirstDepartureDate']")).click();
-
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         fillForm(By.xpath("//input[@data-test-name='FirstDepartureDate']"),setData());
 
-        driver.findElement(By.xpath("//label[@class='btn btn-attention active']")).click();
+        WebElement element3 = driver.findElement(By.xpath("//label[@class='btn btn-attention']"));
+        scrollToElement(element3);
+        click(By.xpath("//label[@class='btn btn-attention']"));
 
-        fillForm(By.xpath("//div[@data-fi-input-mode='combined']"),name);
-        fillForm(By.xpath("//input[@data-test-name=\"BirthDate\"]"),dateOfBirth);
+        fillForm(By.xpath("//input[@data-test-name='FullName']"),"IVANOV  IVAN");
+        fillForm(By.xpath("//input[@data-test-name=\"BirthDate\"]"),"25101994");
 
-        driver.findElement(By.xpath("//*[contains(text(), 'активный отдых или спорт')]/ancestor::div[@class=\"calc-vzr-toggle-risk-group\"]//div[@class=\"toggle off toggle-rgs\"]")).click();
+        WebElement element4 = driver.findElement(By.xpath("//*[contains(text(), 'активный отдых или спорт')]/ancestor::div[@class=\"calc-vzr-toggle-risk-group\"]//div[@class=\"toggle off toggle-rgs\"]"));
+        scrollToElement(element4);
+        checkBoxCheck(element4);
 
-        driver.findElement(By.xpath("//input[@data-test-name='IsProcessingPersonalDataToCalculate']")).click();
+        WebElement element5 = driver.findElement(By.xpath("//label[@class='adaptive-checkbox-label'][contains(text(),' Я согласен')]"));
+        scrollToElement(element5);
+        checkBoxCheck(element5);
 
-        driver.findElement(By.xpath("//button[@data-test-name='NextButton'][contains(text(),'Рассчитать')]")).click();
+        click(By.xpath("//button[@data-test-name='NextButton'][contains(text(),'Рассчитать')]"));
+
+        waitFieldisDisplayed(By.xpath("//div[@class='program-name'][contains(text(),'Комфорт')]"));
+
+        WebElement element6 = driver.findElement(By.xpath("//h2[@class='step-title'][contains(text(),'Расчет')]"));
+        scrollToElement(element6);
 
         compareText(driver.findElement(By.xpath("//span[contains(text(),'Многократные поездки')]")).getText(),"Многократные поездки в течение года");
         compareText(driver.findElement(By.xpath("//strong[contains(text(),'Шенген')]")).getText(),"Шенген");
-        compareText(driver.findElement(By.xpath("//strong[contains(text(),'IVANOV IVAN')]")).getText(),name);
-        compareText(driver.findElement(By.xpath("//strong[contains(text(),'25')]")).getText(),dateOfBirth);
+        compareText(driver.findElement(By.xpath("//strong[contains(text(),'IVANOV IVAN')]")).getText(),"IVANOV IVAN");
+        compareText(driver.findElement(By.xpath("//strong[contains(text(),'25')]")).getText(),"25.10.1994");
         compareText(driver.findElement(By.xpath(" //div[@style=\"visibility: visible; opacity: 1; display: block; transform: translateX(0px);\"]//child::small[@data-bind=\"text: ko.unwrap('undefined' === typeof info ? '' : info)\"]")).getText(),"(включая активный отдых)");
-
         driver.close();
     }
-
 }
